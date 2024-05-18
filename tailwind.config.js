0cb09d8a5e3c2995c6d319c4099d5cdc20296b94
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-05-18 22:13:14 +0545

    changed fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "Sans-serif"],
+        inter: ["Inter", "Sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
