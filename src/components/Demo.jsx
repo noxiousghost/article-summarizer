914297f71af870260e43ab6886ae74e152a72006
NarayanSapkota <59175636+noxiousghost@users.noreply.github.com>
2024-05-28 13:17:44 +0545

    implemented api request through form

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,13 +1,31 @@
 import React, { useEffect, useState } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
+import { useLazyGetSummaryQuery } from "../services/article";
 
 function Demo() {
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
+
+  const handelSUbmit = async (e) => {
+    e.preventDefault();
+    const { data } = await getSummary({ articleUrl: article.url });
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+      setArticle(newArticle);
+      console.log(newArticle);
+    }
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={() => {}}
+          onSubmit={handelSUbmit}
         >
           <img
             src={linkIcon}
@@ -18,8 +36,8 @@ function Demo() {
           <input
             type="url"
             placeholder="Paste the article link"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className="url_input peer" // When you need to style an element based on the state of a sibling element, mark the sibling with the peer class, and use peer-* modifiers to style the target element
           />
